7c9d6dba17f3cae4bed8ddf0755f299f62e92563
Linus Larsson <linus.genuine@gmail.com>
2012-09-05 07:04:01 +0200

    Justerat CSS och fonter

--- iamlinus_se/wp-content/themes/iamlinusv3/header.php
+++ iamlinus_se/wp-content/themes/iamlinusv3/header.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head profile="http://gmpg.org/xfn/1">
-	<title>Linus Larsson, projekt- och produktionsledare inom webbutveckling | iamlinus.se</title>
 	<meta http-equiv="Content-Type" content="text/html;" charset="utf-8" />
-
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Linus Larsson, projekt- och produktionsledare inom webbutveckling | iamlinus.se</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	
 	<!-- 1140px Grid styles for IE -->
 	<!--[if lte IE 9]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" /><![endif]-->
@@ -12,24 +11,39 @@
 	<!-- The 1140px Grid - http://cssgrid.net/ -->
 	<link rel="stylesheet" href="<?php bloginfo('url'); ?>/wp-content/themes/iamlinusv3/1140.css" type="text/css" media="screen" />
 	
-	<!-- Your styles -->
+	<!-- Min CSS -->
 	<link rel="stylesheet" href="<?php bloginfo('url'); ?>/wp-content/themes/iamlinusv3/style.css" type="text/css" media="screen" />
 	
 	<!--css3-mediaqueries-js - http://code.google.com/p/css3-mediaqueries-js/ - Enables media queries in some unsupported browsers-->
 	<script type="text/javascript" src="js/css3-mediaqueries.js"></script>
 
-<link href='http://fonts.googleapis.com/css?family=Ultra' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Puritan:400,700' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Josefin+Slab:300' rel='stylesheet' type='text/css'>
+<!-- Font embed -->
+	<link href='http://fonts.googleapis.com/css?family=Ultra' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Puritan:400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Josefin+Slab:300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Bitter:700' rel='stylesheet' type='text/css'>
 	
-<link rel="shortcut icon" href="" type="image/vnd.microsoft.icon" />
-<link rel="icon" href="" type="image/gif" />
+	<link rel="shortcut icon" href="" type="image/vnd.microsoft.icon" />
+	<link rel="icon" href="" type="image/gif" />
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="" />
 	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php //comments_popup_script(); // off by default ?>
 	<?php wp_head(); ?>
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-10134673-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+	</script>
 </head>
 <div class="header"></div>
   <body>
