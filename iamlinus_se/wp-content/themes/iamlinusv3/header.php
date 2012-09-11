c629e4b9ec074277c6069bf9a434b6b24f3589d0
Linus Larsson <linus.genuine@gmail.com>
2012-09-11 22:23:06 +0200

    Provar tracking alternativ

--- iamlinus_se/wp-content/themes/iamlinusv3/header.php
+++ iamlinus_se/wp-content/themes/iamlinusv3/header.php
@@ -17,7 +17,7 @@
 	<!--css3-mediaqueries-js - http://code.google.com/p/css3-mediaqueries-js/ - Enables media queries in some unsupported browsers-->
 	<script type="text/javascript" src="js/css3-mediaqueries.js"></script>
 
-<!-- Font embed -->
+	<!-- Font embed -->
 	<link href='http://fonts.googleapis.com/css?family=Ultra' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Puritan:400,700' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Josefin+Slab:300' rel='stylesheet' type='text/css'>
@@ -31,19 +31,7 @@
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php //comments_popup_script(); // off by default ?>
 	<?php wp_head(); ?>
-	<script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-10134673-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-	</script>
 </head>
 <div class="header"></div>
   <body>
